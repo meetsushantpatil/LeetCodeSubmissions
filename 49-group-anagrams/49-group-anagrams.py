d70cf734b92f72bb70e5380e955033b363f6b759
Sushant Patil <meetsushantpatil@gmail.com>
2022-07-17 21:45:07 -0400

    Time: 106 ms (91.59%), Space: 17.1 MB (88.39%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,27 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    
+        string_dict = {}
+        sorted_list = []
+                
+        for i in strs :
+            sorted_list.append(''.join(sorted(i)))
+            
+        # string_set = set(sorted_list)
+        # result = [""]*len(string_set)
+        
+        if(len(sorted_list)==2):
+            if(sorted_list[0]==sorted_list[1]):
+                return [[strs[0],strs[1]]]
+            else:
+                return [[strs[0]],[strs[1]]]
+        
+        for i in range(0,len(sorted_list)):
+            if sorted_list[i] not in string_dict:
+                string_dict[sorted_list[i]] = [strs[i]]
+            else:
+                string_dict[sorted_list[i]].append(strs[i])
+                        
+                        
+                    
+        return list(string_dict.values())

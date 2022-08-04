cdadb156607583aab3ad5b8a39447cad1c087753
Sushant Patil <meetsushantpatil@gmail.com>
2022-08-03 23:26:40 -0400

    Time: 56 ms (86.12%), Space: 14.7 MB (52.38%) - LeetHub

--- 818-race-car/818-race-car.py
+++ 818-race-car/818-race-car.py
@@ -1,12 +1,15 @@
 class Solution: # worst case time o(nlogn) - position range n nodes and speed*2 every time so logn
     def racecar(self, target: int) -> int: # space o(nlogn) because number of nodes in graph matches time    
-        queue = []
+        queue, visited = [], set()
         position, speed, count = 0, 1, 0
         queue.append([position, speed, count])
         while queue: # bfs with pruning
             p, s, c = queue.pop(0)
+            if (p, s) in visited: # if visited same position with same speed
+                continue # skip searching it
             if p == target: # if reached target
                 return c # return count
+            visited.add((p, s)) # add new state to visited set
             if abs(target-(p+s)) < target: # if the next position does not further us from the target
                 queue.append([p+s, s*2, c+1]) # then keep the car in the direction it is going
             if p+s > target and s > 0: # if car passed the target and moving forward

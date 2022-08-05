fa4bd74b2c464ab2dc9261a2634cde7b96489f4f
Sushant Patil <meetsushantpatil@gmail.com>
2022-08-04 22:30:40 -0400

    Time: 30 ms (95.44%), Space: 13.9 MB (61.33%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.py
+++ 362-design-hit-counter/362-design-hit-counter.py
@@ -9,16 +9,26 @@ class HitCounter:
         self.list.append(timestamp)
 
     def getHits(self, timestamp: int) -> int:
-        i = 0
+        left, right = 0, len(self.list)-1
+        target = timestamp - 300
         
-        while(i<len(self.list)):
-            if(timestamp>300):
-                if(self.list[i]<=(timestamp-300)): # [1,2,3,4,300]
-                    del self.list[i]
-                    i-=1
-            i+=1
+        while(left<=right):
+            mid = right-left//2
+            if(self.list[mid]<=target):
+                left = mid+1
+            else:
+                right = mid-1
+        
+        return len(self.list)-left
+        
+#         while(i<len(self.list)):
+#             if(timestamp>300):
+#                 if(self.list[i]<=(timestamp-300)): # [1,2,3,4,300]
+#                     del self.list[i]
+#                     i-=1
+#             i+=1
             
-        return len(self.list)
+#         return len(self.list)
 
 
 # Your HitCounter object will be instantiated and called as such:

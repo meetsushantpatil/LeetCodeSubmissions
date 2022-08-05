55f230f86de472031cebe39de8cdd84886ccc08e
Sushant Patil <meetsushantpatil@gmail.com>
2022-08-05 00:00:37 -0400

    Time: 1035 ms (67.69%), Space: 38.3 MB (58.52%) - LeetHub

---
+++ 1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.py
@@ -0,0 +1,29 @@
+# {"daniel" : [10:00, 10:05, 10:10]}
+
+from collections import defaultdict
+
+class Solution:
+    def alertNames(self, keyName: List[str], keyTime: List[str]) -> List[str]:
+        log = defaultdict(list)
+        result = []
+        
+        for i in range(len(keyName)):
+            log[keyName[i]].append(int(keyTime[i][0:2]+keyTime[i][3:5]))
+            
+        
+        
+        for key in log:
+            log[key].sort()
+        
+        print(log)
+        
+        for key in log:
+            
+            for i in range(2, len(log[key])):
+                if log[key][i] - log[key][i-2] <= 100:
+                    result.append(key)
+                    break
+        
+        result.sort()
+        return result
+                

f8c2ead927ca275b22a29e4915d35dc535ef51d5
Sushant Patil <meetsushantpatil@gmail.com>
2022-08-06 16:57:29 -0400

    Time: 63 ms (82.34%), Space: 13.9 MB (82.34%) - LeetHub

--- 811-subdomain-visit-count/811-subdomain-visit-count.py
+++ 811-subdomain-visit-count/811-subdomain-visit-count.py
@@ -9,33 +9,47 @@ class Solution:
             visit_count = int(visit_list[0])
             domains = visit_list[1].split(".")
             
-            if(len(domains)==2):
-                if(domains[1] not in visit_dict):
-                    visit_dict[domains[1]] = visit_count
-                else:
-                    visit_dict[domains[1]] += visit_count
+            for i in range(len(visit_list[1])-1,-1,-1):
+                domain_indv = ""
+                
+                if(visit_list[1][i]=="."):
+                    domain_indv = visit_list[1][i+1:len(visit_list[1])]
                     
-                if(domains[0]+"."+domains[1] not in visit_dict):    
-                    visit_dict[domains[0]+"."+domains[1]] = visit_count
-                else:
-                    visit_dict[domains[0]+"."+domains[1]] += visit_count
+                if(i==0):
+                    domain_indv = visit_list[1]
+                
+                if(domain_indv not in visit_dict and domain_indv!=""):
+                    visit_dict[domain_indv] = visit_count
+                elif(domain_indv in visit_dict):
+                    visit_dict[domain_indv] += visit_count
+            
+#             if(len(domains)==2):
+#                 if(domains[1] not in visit_dict):
+#                     visit_dict[domains[1]] = visit_count
+#                 else:
+#                     visit_dict[domains[1]] += visit_count
                     
-            if(len(domains)==3):
-                if(domains[2] not in visit_dict):
-                    visit_dict[domains[2]] = visit_count
-                else:
-                    visit_dict[domains[2]] += visit_count
+#                 if(domains[0]+"."+domains[1] not in visit_dict):    
+#                     visit_dict[domains[0]+"."+domains[1]] = visit_count
+#                 else:
+#                     visit_dict[domains[0]+"."+domains[1]] += visit_count
                     
-                if(domains[1]+"."+domains[2] not in visit_dict):    
-                    visit_dict[domains[1]+"."+domains[2]] = visit_count
-                else:
-                    visit_dict[domains[1]+"."+domains[2]] += visit_count
+#             if(len(domains)==3):
+#                 if(domains[2] not in visit_dict):
+#                     visit_dict[domains[2]] = visit_count
+#                 else:
+#                     visit_dict[domains[2]] += visit_count
+                    
+#                 if(domains[1]+"."+domains[2] not in visit_dict):    
+#                     visit_dict[domains[1]+"."+domains[2]] = visit_count
+#                 else:
+#                     visit_dict[domains[1]+"."+domains[2]] += visit_count
                     
                 
-                if(domains[0]+"."+domains[1]+"."+domains[2] not in visit_dict):    
-                    visit_dict[domains[0]+"."+domains[1]+"."+domains[2]] = visit_count
-                else:
-                    visit_dict[domains[0]+"."+domains[1]+"."+domains[2]] += visit_count
+#                 if(domains[0]+"."+domains[1]+"."+domains[2] not in visit_dict):    
+#                     visit_dict[domains[0]+"."+domains[1]+"."+domains[2]] = visit_count
+#                 else:
+#                     visit_dict[domains[0]+"."+domains[1]+"."+domains[2]] += visit_count
                     
         result = []
         for key in visit_dict:

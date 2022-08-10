4784c161ea7a3c845fec3aa1a0bca04e4a725406
Sushant Patil <meetsushantpatil@gmail.com>
2022-08-10 17:58:54 -0400

    Time: 905 ms (22.65%), Space: 14.7 MB (92.29%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -0,0 +1,28 @@
+class MyCalendar:
+
+    def __init__(self):
+        self.event_list = []
+
+    def book(self, start: int, end: int) -> bool:
+        
+        if(len(self.event_list)==0):
+            self.event_list.append([start,end])
+            return True
+        else:
+            for start_temp,end_temp in self.event_list:
+                if(start>=start_temp and start<end_temp):
+                    return False
+                elif(end>start_temp and end<=end_temp):
+                    return False
+                elif(start<start_temp and end>=end_temp):
+                    return False
+            
+            self.event_list.append([start,end])
+            return True
+            
+            
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)  10,20
+# param_1 = obj.book(start,end)  15,25

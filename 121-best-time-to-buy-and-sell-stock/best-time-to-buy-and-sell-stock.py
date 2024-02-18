38423b81ab362650c6fd88457f37a950ec717849
Sushant Patil <meetsushantpatil@gmail.com>
2024-02-18 14:45:39 -0500

    Time: 677 ms (97.67%) | Memory: 27.5 MB (67.51%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,7 +1,7 @@
 # REACTO - https://www.youtube.com/watch?v=DIR_rxusO8Q&t=87s 
 # Repeat - Given a list of stock price, find the max profit you can make. 
 # Clarifying questions - 1. Can you sell before you buy? 2. Can you have 0 profits? 
-# Examples - 1. [1,4,7] -> 6, [6,5,1] -> 0, [1] -> 0
+# Examples - 1. [1,4,7] -> 6, [6,5,1] -> 0, [1] -> 0, [] -> 0
 # Approach -   1. 
 # find minimum value, keep max_profit, return the value -> Complexity O (n)
 # Code
@@ -13,13 +13,13 @@ class Solution:
         
         min_price = float('inf') 
         max_profit = 0
-        for price in prices: # [1,4,7], [6,5,1]
+        for price in prices: # Testing - [1,4,7], [6,5,1], [1], []
             if price < min_price: 
-                min_price = price # [1], [1]
+                min_price = price # Testing - [1, 1, 1], [6,5,1], [1]
             
-            temp_max_profit =  price - min_price # [6], [-5]
+            temp_max_profit =  price - min_price # testing = [0,3,6], [0, -1, -5], [0]
             if temp_max_profit > max_profit:
-                max_profit = temp_max_profit # [6], [0]
+                max_profit = temp_max_profit # Testing = [0,3,6], [0,0,0], [0]
 
-        return max_profit
+        return max_profit # returns 0 for empty list
            

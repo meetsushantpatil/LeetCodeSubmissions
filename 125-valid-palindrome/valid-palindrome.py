8ed34f1058f612a50a5aa11bb21305bc78cd703c
Sushant Patil <meetsushantpatil@gmail.com>
2024-02-18 15:37:26 -0500

    Time: 50 ms (36.75%) | Memory: 17 MB (81.58%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,7 +3,7 @@
 # Clarifying questions - 1. Will the string contain non-alphanumeric letters? 2. Case-sensitive?
 # Examples - 1. "race a car" -> True, 2. "tst" -> True 3. "#abcba" -> True 4. ""  -> True 5. " " -> True
 # Approach -   
-# Keep 2 pointers, left & right. If it finds non-alphanumeric, go ahead for left or right. If matches, go ahead, else break && return False.
+# Keep 2 pointers, left & right. If it finds non-alphanumeric, go ahead for left or right. If matches, go ahead, else break && return False. -> O(n)
 # Code
 # Test
 # Optimisation
@@ -11,11 +11,11 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
-        if len(s) < 2:  # ["tsT"],  ["car ac"], ["#*aba"], "c"
+        if len(s) < 2:  
             return True
         
-        left_ptr = 0 # 0, 0, 0, 0
-        right_ptr = len(s)-1 # 2, 5, 4, 29
+        left_ptr = 0 
+        right_ptr = len(s)-1 
 
         while left_ptr < right_ptr:  
 
@@ -33,9 +33,3 @@ class Solution:
                 right_ptr-= 1
 
         return True
-
-
-
-
-
-        

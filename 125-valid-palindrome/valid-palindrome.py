8f495dbaa043ced37128f727b83be2ee8a156e75
Sushant Patil <meetsushantpatil@gmail.com>
2024-02-18 15:36:47 -0500

    Time: 48 ms (45.96%) | Memory: 17 MB (81.58%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,41 @@
+# REACTO - https://www.youtube.com/watch?v=DIR_rxusO8Q&t=87s 
+# Repeat - Given a string, tell me if it is a palindrome.
+# Clarifying questions - 1. Will the string contain non-alphanumeric letters? 2. Case-sensitive?
+# Examples - 1. "race a car" -> True, 2. "tst" -> True 3. "#abcba" -> True 4. ""  -> True 5. " " -> True
+# Approach -   
+# Keep 2 pointers, left & right. If it finds non-alphanumeric, go ahead for left or right. If matches, go ahead, else break && return False.
+# Code
+# Test
+# Optimisation
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        if len(s) < 2:  # ["tsT"],  ["car ac"], ["#*aba"], "c"
+            return True
+        
+        left_ptr = 0 # 0, 0, 0, 0
+        right_ptr = len(s)-1 # 2, 5, 4, 29
+
+        while left_ptr < right_ptr:  
+
+            if s[left_ptr].isalnum() and s[right_ptr].isalnum():
+                if s[left_ptr].lower() != s[right_ptr].lower():
+                    return False
+                else:
+                    left_ptr+= 1
+                    right_ptr-= 1 
+            
+            if not s[left_ptr].isalnum():
+                left_ptr+= 1
+            
+            if not s[right_ptr].isalnum():
+                right_ptr-= 1
+
+        return True
+
+
+
+
+
+        
